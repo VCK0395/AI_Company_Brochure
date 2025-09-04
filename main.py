1a339d2f363069c830372ea4278d7bbb1eadcc03
Victor Kalu <87344807+VCK0395@users.noreply.github.com>
2025-09-04 02:06:33 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,90 @@
+# imports
+
+import os
+import requests
+from bs4 import BeautifulSoup
+from typing import List
+from dotenv import load_dotenv
+from openai import OpenAI
+import google.generativeai
+import anthropic
+
+# Load environment variables in a file called .env
+# Print the key prefixes to help with any debugging
+
+  google_api_key = genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+
+if google_api_key:
+    print(f"Google API Key exists and begins {google_api_key[:8]}")
+else:
+    print("Google API Key not set")
+
+google.generativeai.configure()
+
+
+
+def stream_gpt(prompt):
+    messages = [
+        {"role": "system", "content": system_message},
+        {"role": "user", "content": prompt}
+      ]
+  # The system message is passed as a 'system_instruction' to the model.
+    model = genai.GenerativeModel(
+        model_name='gemini-2.5-flash', # A fast and capable model, similar to gpt-4o-mini.
+        system_instruction=system_instruction
+    )
+
+    # The user prompt is passed to generate_content().
+    completion = model.generate_content(user_prompt_content)
+
+  
+    stream = openai.chat.completions.create(
+        model='gpt-4o-mini',
+        messages=messages,
+        stream=True
+    )
+    result = ""
+    for chunk in stream:
+        result += chunk.choices[0].delta.content or ""
+        yield result
+# A class to represent a Webpage
+
+# Some websites need you to use proper headers when fetching them:
+headers = {
+ "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
+# A class to represent a Webpage
+
+class Website:
+    url: str
+    title: str
+    text: str
+
+    def __init__(self, url):
+        self.url = url
+        response = requests.get(url)
+        self.body = response.content
+        soup = BeautifulSoup(self.body, 'html.parser')
+        self.title = soup.title.string if soup.title else "No title found"
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        self.text = soup.body.get_text(separator="\n", strip=True)
+
+    def get_contents(self):
+        return f"Webpage Title:\n{self.title}\nWebpage Contents:\n{self.text}\n\n"
+
+system_message = "You are an assistant that analyzes the contents of a company website landing page \
+and creates a short brochure about the company for prospective customers, investors and recruits. Respond in markdown."
+
+def stream_brochure(company_name, url, model):
+    yield ""
+    prompt = f"Please generate a company brochure for {company_name}. Here is their landing page:\n"
+    prompt += Website(url).get_contents()
+    if model=="GPT":
+        result = stream_gpt(prompt)
+    else:
+        raise ValueError("Unknown model")
+    yield from result
+
+  
